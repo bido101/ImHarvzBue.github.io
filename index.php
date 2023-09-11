70a71f96c4ea2f6a3855270ff85f5adfde78d0f2
harvey <40221196+bido101@users.noreply.github.com>
2023-09-11 14:09:40 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -55,13 +55,179 @@
             </div>
         </header>
         <!-- About-->
-        <?php include 'https://bido101.github.io/ImHarvzBue.github.io/dashboard_components/profile_about.php'; ?>
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">Welcome to my portfolio</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4">
+                            I'am a freelane <b>FULL-STACK WEB DEVELOPER</b> since 2019, I earned my Bachelor’s Degree in Information Technology at Eastern Visayas State University (EVSU). I wish to bring my knowledge, skills and commitment to excellence to your Company's innovative environment.
+                            
+                            I am currently working at National Maritime Polytechnic as Information Technologist Support/Job Order Worker for almost 2 years. My internship at Eastern Visayas State University (EVSU) ICT DEPARTMENT. I had the opportunity to work with seasoned professionals who taught me how to easily grasp complex situations and at certain times, how to roll with the punches in order to achieve various objectives. I am a freelance programmer for 5 years in which I have encountered many projects and I am willing to share my experience with you.
+                        </p>
+                        <a class="btn btn-light btn-xl" href="#accomplishment">Get Started!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
         <!-- accomplishment -->
-        <?php include_once 'https://bido101.github.io/ImHarvzBue.github.io/dashboard_components/accomplishment_section.php'; ?>
+        <div id="accomplishment">
+            <section class="page-section">
+                <div class="container px-4 px-lg-5">
+                    <h2 class="text-center mt-0">Accomplishment</h2>
+                    <hr class="divider"/>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="row gx-4 gx-lg-5">
+                                <div class="card" style="width: 33rem;">
+                                  <img src="assets/img/Screenshot 2023-09-07 144907.png" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <label>
+                                        <b>Capstone Project:</b> <u>EVSU General Maintenance with SMS Notification</u> 
+                                    </label>
+                                    <hr>
+                                    <p class="card-text" style="font-size:14px;">
+                                        The proposed study aims to bring a solution to the manual transactions being made under General Maintenance Division among of this Division of its clientele under Academic Unit and Non-Academic Unit.
+                                    </p>
+                                  </div>
+                                </div>
+                            </div>    
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="row gx-4 gx-lg-5">
+                                <div class="card" style="width: 33rem;">
+                                  <img src="assets/img/Screenshot 2023-09-07 150432.png" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                    <label>
+                                        <b>Capstone Project:</b> <u>Villaconzoilo Farm</u> 
+                                    </label>
+                                    <hr>
+                                    <p class="card-text" style="font-size:14px;">
+                                        This study aims to design and develop web-based farm management for Villaconzoilo Farm which automates the management and operations of the Farm.
+                                    </p>
+                                  </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        
+                </div>
+            </section>
+        </div>
         <!-- skills and experience-->
-        <?php include_once 'https://bido101.github.io/ImHarvzBue.github.io/dashboard_components/skills_experiences.php'; ?>
+        <section class="page-section" id="skills_experience">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Skills/Experience</h2>
+                <hr class="divider" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-filetype-html fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">HTML</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-filetype-php fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">PHP Language</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-filetype-sql fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">MYSQL</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-bootstrap fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Bootstrap</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-filetype-css fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">CSS</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-filetype-js fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Javascript</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <!-- Contact-->
-        <?php include_once 'https://bido101.github.io/ImHarvzBue.github.io/dashboard_components/contact_section.php' ?>
+        <section class="page-section" id="contact">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 col-xl-6 text-center">
+                        <h2 class="mt-0">Let's Get In Touch!</h2>
+                        <hr class="divider" />
+                        <p class="text-muted mb-5">Ready to start your next project with me? Send us a messages and we will get back to you as soon as possible!</p>
+                    </div>
+                </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
+                    <div class="col-lg-6">
+                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                            <!-- Name input-->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+                                <label for="name">Full name</label>
+                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                            </div>
+                            <!-- Email address input-->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                                <label for="email">Email address</label>
+                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                            </div>
+                            <!-- Phone number input-->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
+                                <label for="phone">Phone number</label>
+                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                            </div>
+                            <!-- Message input-->
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
+                                <label for="message">Message</label>
+                                <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                            </div>
+                            <!-- Submit success message-->
+                            <!---->
+                            <!-- This is what your users will see when the form-->
+                            <!-- has successfully submitted-->
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    To activate this form, sign up at
+                                    <br />
+                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div>
+                            <!-- Submit error message-->
+                            <!---->
+                            <!-- This is what your users will see when there is-->
+                            <!-- an error submitting the form-->
+                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                            <!-- Submit Button-->
+                            <div class="d-grid"><button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Submit</button></div>
+                        </form>
+                    </div>
+                    <div class="col-lg-5 mb-8 mb-lg-0">
+                        <i class="bi bi-envelope-at fs-5 mb-4 text-muted"> harvz.buena@gmail.com</i><br><br>
+                        <i class="bi bi-linkedin fs-6 mb-4 text-muted"> www.linkedin.com/in/harvey-buena-b74139275</i><br><br>
+                        <i class="bi-phone fs-5 mb-4 text-muted"> +639359452475</i>
+                    </div>
+                </div>
+            </div>
+        </section>
         <!-- Bootstrap core JS-->
         <script src="https://bido101.github.io/ImHarvzBue.github.io/assets/js/bootstrap.bundle.min.js"></script>
         <!-- SimpleLightbox plugin JS-->
